2047ad7cdaa56cc76c16d7cecdae823917e285b5
Damilola-FullStackDeveloper <bakarewealth@gmail.com>
2024-04-22 16:30:29 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const data  = [
+    { 
+        name: "Michael",
+        age: 45,
+        city: "London",
+        profession: "Engineer"
+    },
+    { 
+        name: "Emily",
+        age: 32,
+        city: "Paris",
+        profession: "Artist"
+    },
+    { 
+        name: "David",
+        age: 50,
+        city: "Berlin",
+        profession: "Doctor"
+    },
+    { 
+        name: "Sophia",
+        age: 28,
+        city: "Tokyo",
+        profession: "Teacher"
+    }
+];
