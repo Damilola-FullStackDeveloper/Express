203e2ecd6a2dde7c13158169483895d6a6a95b8b
Damilola-FullStackDeveloper <bakarewealth@gmail.com>
2024-04-25 17:34:05 +0100

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import { data } from './data.js';
-import cors from './cors';
+import cors from 'cors'
+import dotenv from 'dotenv/config.js'
+import userRoutes from './routes/users.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 10000;
-
-
+app.use(bodyParser.json());
 app.use(cors())
+app.use('/api', userRoutes) 
 app.get('/home', (req, res)=>{
    res.send(data)
 })
---
+++ routes/users.js
@@ -0,0 +1,28 @@
+import express from "express"
+import db from "../db/conn.js"
+const userRoutes = express.Router()
+
+userRoutes.get("/users", async(req, res) =>{
+   let  collection = await db.collection("smokey")
+   let results = await collection.find({}).limit(20).toArray()
+   res.send(results)
+})
+
+
+userRoutes.post("/users", async(req, res) =>{
+   let data = req.body
+   if(!data) res.send("please send something").status(400)
+    data.data = new Date()
+   let collection = await db.collection("smokey-black-bears")
+   let results = await collection.insertOne(data)
+
+   if(results){
+      res.send('user saved successfully').status(201)
+   }else if(!results){
+      res.send('server error').status(500)
+   }
+
+})
+
+export default userRoutes;
+
